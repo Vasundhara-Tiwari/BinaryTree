08e54238efa4626069659c30254ae478012ef04b
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-30 10:19:13 +0530

    Creating a class TreeNode in BinaryTree

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,5 +1,11 @@
 package tree;
 
 public class BinaryTree {
+    private TreeNode root;
 
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+    }
 }
