c570abbf0d6c2a3580f33883764964e6dd9aa4fe
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-30 10:25:37 +0530

    calling the method of class BinaryTree from main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import tree.BinaryTree;
+
 public class Main {
     public static void main(String[] args) {
-
+        BinaryTree bt = new BinaryTree();
+        bt.createBinaryTree();
     }
 }
