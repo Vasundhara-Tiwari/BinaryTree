03501b04f0b264fb5406983bc5b937aa227a2b47
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-30 10:24:21 +0530

    Creating a method create binary tree

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -11,5 +11,20 @@ public class BinaryTree {
             this.data = data;
         }
     }
-
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+        TreeNode sixth = new TreeNode(6);
+        TreeNode seventh = new TreeNode(7);
+        root = first;
+        first.left = second;
+        first.right = third;
+        second.left = fourth;
+        second.right = fifth;
+        third.left = sixth;
+        third.right = seventh;
+    }
 }
